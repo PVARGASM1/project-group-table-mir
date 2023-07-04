b5599ff86adc14ee4cce083c4f7c05ab6561aab1
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-07-04 17:56:20 -0500

    First steps layout desing

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='title'>My Table</h1>
+    </header>
+  )
+}
